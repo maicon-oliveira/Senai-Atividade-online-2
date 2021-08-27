dd42d5cdfdcb02f4c675748d3f60881368dc8d55
maicon-oliveira <maicon.oliveira0702@gmail.com>
2021-08-26 22:46:30 -0300

    save para github

---
+++ cadastroEventos.js
@@ -0,0 +1,29 @@
+let idade = 19
+
+
+if (idade > 18){
+    console.log("Cadastro Autorizado.");
+} else {
+    console.log("Cadastro Não é Permitido Pela Idade.");
+}
+
+
+let quantParticipantes = 99
+
+if (quantParticipantes < 100){
+    console.log("Você esta Apto a Participar do Evento.");
+} else {
+    console.log("Cadastro Não Permitido, Limite de Participantes Excedido");
+}
+
+let dataEvento = 31/08/2021
+let listaParticipantes = ["Maicon ", " Diego ", " Ruan ", " Emanuel ", " Flávio ", " Renan ."]
+let listaPalestrantes = ["Thiago Nascimento " , " Odirlei Assis ."]
+
+if (dataEvento > 26/08/2021){
+    console.log("Cadastro e evento realizados com sucesso!");
+    console.log("Participantes do Evento : " + listaParticipantes);
+    console.log("Palestrantes do Evento : " + listaPalestrantes);
+} else {
+    console.log("Cadastro não permitido por data inválida.");
+}
