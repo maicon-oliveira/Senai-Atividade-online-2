9346a2c5cd7a7eedb87301c67649e51a08aa00ae
maicon-oliveira <maicon.oliveira0702@gmail.com>
2021-08-29 16:39:50 -0300

    criacao da function formDate

--- cadastroEventos.js
+++ cadastroEventos.js
@@ -15,15 +15,27 @@ if (quantParticipantes < 100){
 } else {
     console.log("Cadastro Não Permitido, Limite de Participantes Excedido");
 }
+ function formDate(data = new Date()){
+     var dia = data.getDate();
+     var mes = data.getMonth() ;
+     var ano = data.getFullYear();
 
-let dataEvento = 31/08/2021
-let listaParticipantes = ["Maicon ", " Diego ", " Ruan ", " Emanuel ", " Flávio ", " Renan ."]
-let listaPalestrantes = ["Thiago Nascimento " , " Odirlei Assis ."]
+     if(dia.toString().length == 1) dia = '0' + dia;
+     if(mes.toString().length == 1) mes = '0' + mes;
 
-if (dataEvento > 26/08/2021){
+     return dia+'/'+mes+'/'+ano;
+
+    }
+let dataEvento = 30/10/2021;
+let listaParticipantes = ["Maicon ", " Diego ", " Ruan ", " Emanuel ", " Flávio ", " Renan ."];
+let listaPalestrantes = ["Thiago Nascimento " , " Odirlei Assis ."];
+
+  
+if (dataEvento > formDate()){
     console.log("Cadastro e evento realizados com sucesso!");
     console.log("Participantes do Evento : " + listaParticipantes);
     console.log("Palestrantes do Evento : " + listaPalestrantes);
 } else {
     console.log("Cadastro não permitido por data inválida.");
 }
+
